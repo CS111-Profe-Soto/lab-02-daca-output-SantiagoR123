109a366379b498ce895689bfa7684fc52f76235c
SantiagoR123 <Santiagorosasmariano@gmail.com>
2026-02-21 23:37:18 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,18 +7,38 @@ public class Main
 	public static void main(String[] args)
 	{
 		//DECLARATION + INITIALIZATION SECTION
-		
+		String surname, givenName, category, cardNum, birthCountry, termsAndConditions;
+		char sex;
+		Strring birthMonth;
+		int uscisNum1, uscisNum2, uscisNum3, birthDay, birthYear, validDay, validMonth, validYear, expireMonth, expireDay, expireYear;
+		surname = "ROSAS";
+		givenName = "Santiago";
+		category = "C09";
+		CardNum = "SRC1234567890";
+		birthCountry = "United States of America";
+		termsAndConditions = "None";
+		sex = 'M' // sex is a character so single quotes are requiered
+		uscisNum1 = 123;
+		uscisNum2 = 456;
+		uscisNum3 = 789;
+		birthDay = 22;
+		birthYear = 2005;
+		validDay = 2;
+		validMonth = 2;
+		validYear = 2027;
+		expireMonth = 2;
+		expireDay = 2;
+		expireYear = 2029;
 		//INPUT SECTION
 		// N/A
-		
 		//CALCULATION SECTION
-		
+		//N/A
 		//OUTPUT SECTION
 		System.out.println("╔══════════════════════════════════════════════════════════════════════");
 		System.out.println("║         UNITED STATES OF AMERICA");
 		System.out.println("║                                  EMPLOYMENT AUTHORIZATION CARD");
 		System.out.println("║                       Surname");
-		System.out.println("║                       ███████ ");
+		System.out.println("║                       " + surname + " ");
 		System.out.println("║  .----.    .----.     Given Name");
 		System.out.println("║ (   ^  \\  /  ^   )    ███████ ");
 		System.out.println("║        |  |           USCIS#         Category   Card#");
